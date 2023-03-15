a7be9ce88a6a075296f28970e3e13f6774ba89e1
Abel Shibabaw <abelshibabaw@heuc.edu.et>
2023-03-15 22:42:57 +0300

    variables and functions commit

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-alert("Hello World");
+let todo1 = 'Get grocieries';
+let todo2 = 'Clean room';
+let todo3 = 'Do laundry';
+let todo4 = 'Go to the gym';
+
+function addTodo(todoTitle) {
+    let element = document.createElement('div');
+    element.innerText = todoTitle;
+    document.body.appendChild(element);
+}
+addTodo(todo1);
+addTodo(todo2);
+addTodo(todo3);
+addTodo(todo4);
