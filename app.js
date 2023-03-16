1fb8501d5acb0db11e9f63ad544df2cc42a784a1
Abel Shibabaw <abelshibabaw@heuc.edu.et>
2023-03-16 20:54:44 +0300

    variable assignment and reseting function added

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 let todos = ['Get grocieries', 'Clean room', 'Do laundry', 'Go to the gym'];
 
-todos.forEach(function(todoTitle) {
-    let element = document.createElement('div');
-    element.innerText = todoTitle;
-    document.body.appendChild(element);
-});
+
+function render() {
+    document.getElementById('todo-list').innerHTML = '';
+    todos.forEach(function(todoTitle) {
+        const element = document.createElement('div');
+        element.innerText = todoTitle;
+        const todoList = document.getElementById('todo-list');
+        todoList.appendChild(element);
+    });
+}
+render();
+
+function addButton() {
+    const textbox = document.getElementById('text-input');
+    const title = textbox.value;
+    todos.push(title);
+    render();
+}
